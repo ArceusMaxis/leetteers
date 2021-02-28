a0dd5ddc82b229799d68fd82430ae7230f46d709
ArceusMaxis <76719897+ArceusMaxis@users.noreply.github.com>
2021-02-28 11:41:32 +0530

    Add files via upload

---
+++ darkmode.js
@@ -0,0 +1,6 @@
+function d_m() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+console.log("Made by Amirtha Krishnan.");
+console.log("Reach out to me at @amirthaz on Instagram.")
